b0e00a99b3ca3ece4728483861dd48a5411ebaa8
hrscy <15399070815@163.com>
2016-03-28 22:51:22 +0800

    添加导航条

--- SwiftWeibo/Classes/Home/Controller/YJHomeTableViewController.swift
+++ SwiftWeibo/Classes/Home/Controller/YJHomeTableViewController.swift
@@ -16,79 +16,37 @@ class YJHomeTableViewController: YJBaseTableViewController {
         //如果没有登录,设置未登录的信息
         if !userLogin {
             visitorView?.setupVistorViewInfo(true, imageName: "visitordiscover_feed_image_house", message: "关注一些人，回这里看看有什么惊喜")
+            return
         }
+        //初始化导航条
+        setupNav()
+    }
+    
+    //MARK: -初始化导航条
+    private func setupNav() {
+//        //左边按钮
+//        let leftBtn = UIButton()
+//        leftBtn.setImage(UIImage(named: "navigationbar_friendattention"), forState: UIControlState.Normal)
+//        leftBtn.setImage(UIImage(named: "navigationbar_friendattention_highlighted"), forState: UIControlState.Highlighted)
+//        leftBtn.sizeToFit()
+//        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftBtn)
+//        //右边按钮
+//        let rightBtn = UIButton()
+//        rightBtn.setImage(UIImage(named: "navigationbar_pop"), forState: UIControlState.Normal)
+//        rightBtn.setImage(UIImage(named: "navigationbar_pop_highlighted"), forState: UIControlState.Highlighted)
+//        rightBtn.sizeToFit()
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: rightBtn)
+        navigationItem.leftBarButtonItem = createBarButtonItem("navigationbar_friendattention")
+        navigationItem.rightBarButtonItem = createBarButtonItem("navigationbar_pop")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func createBarButtonItem(imageName: String) -> UIBarButtonItem {
+        let btn = UIButton()
+        btn.setImage(UIImage(named: imageName), forState: UIControlState.Normal)
+        btn.setImage(UIImage(named: imageName + "_highlighted"), forState: UIControlState.Highlighted)
+        btn.sizeToFit()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: btn)
+        return UIBarButtonItem(customView: btn)
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- SwiftWeibo/Classes/Main/Controller/YJBaseTableViewController.swift
+++ SwiftWeibo/Classes/Main/Controller/YJBaseTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class YJBaseTableViewController: UITableViewController, YJVistorViewDelegate {
     //定义一个变量，保存用户当前是否登录
-    var userLogin = false
+    var userLogin = true
     //定义属性保存未登录界面
     var visitorView: YJVistorView?
     override func loadView() {
